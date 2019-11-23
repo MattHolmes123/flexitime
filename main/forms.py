e222e8da3771ae83217dd88d743f3103130fb39c
Matthew Holmes <matt_holmes123@hotmail.com>
2019-11-23 12:22:06 +0000

    Add basic Bulma styling to site (including navigation / form display).

--- main/forms.py
+++ main/forms.py
@@ -3,12 +3,19 @@ from django import forms
 from .models import FlexiTimeLog
 
 
+def time_input_kwargs():
+    return {
+        'attrs': {'type': 'time', 'class': 'input'},
+        'format': '%H:%M'
+    }
+
+
 class EditFlexiTimeForm(forms.ModelForm):
     class Meta:
         model = FlexiTimeLog
         fields = ('logged_in', 'break_duration', 'logged_out')
         widgets = {
-            'logged_in': forms.TimeInput(format='%H:%M'),
-            'break_duration': forms.TimeInput(format='%H:%M'),
-            'logged_out': forms.TimeInput(format='%H:%M'),
+            'logged_in': forms.TimeInput(**time_input_kwargs()),
+            'break_duration': forms.TimeInput(**time_input_kwargs()),
+            'logged_out': forms.TimeInput(**time_input_kwargs()),
         }
