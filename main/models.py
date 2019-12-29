a20f905264d833e072511da020027eab6ed677fd
Matthew Holmes <matt_holmes123@hotmail.com>
2019-12-29 11:24:08 +0000

    Add default logged_in value and format FlexiTimeLog create view template.

--- main/models.py
+++ main/models.py
@@ -4,13 +4,35 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
+
+
+def time_now():
+    """Function that returns a rounded login time.
+    Rounds in 5 minute increments.
+
+    :return: time now
+    :rtype: datetime.time
+    """
+
+    now = timezone.now()
+    minutes = now.minute
+
+    remainder = minutes % 5
+
+    if remainder > 0:
+        add_on = datetime.timedelta(minutes=(5-remainder))
+    else:
+        add_on = datetime.timedelta(0)
+
+    return (now + add_on).time()
 
 
 class FlexiTimeLog(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     log_date = models.DateField(default=datetime.date.today)
-    logged_in = models.TimeField()
+    logged_in = models.TimeField(default=time_now)
     break_duration = models.TimeField(null=True, blank=True)
     logged_out = models.TimeField(null=True, blank=True)
 
