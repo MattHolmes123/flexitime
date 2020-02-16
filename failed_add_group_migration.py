c96442fe14bb501e0a856db91008dfb9bf2c1845
Matthew Holmes <matt_holmes123@hotmail.com>
2020-02-16 12:04:08 +0000

    Fix setup-local.sh and run-test.sh by moving migration to a command.

---
+++ failed_add_group_migration.py
@@ -0,0 +1,41 @@
+# from django.db import migrations
+#
+# # See below page for example of forwards_func / reverse_func
+# # https://docs.djangoproject.com/en/3.0/ref/migration-operations/#runpython
+#
+# # Also have a read of this:
+# # https://docs.djangoproject.com/en/2.2/ref/contrib/contenttypes/
+#
+#
+# def forwards_func(apps, schema_editor):
+#     Group = apps.get_model("auth", "Group")
+#     Permission = apps.get_model("auth", "Permission")
+#
+#     all_logs_perm = Permission.objects.get(codename='view_all_user_logs')
+#
+#     db_alias = schema_editor.connection.alias
+#
+#     manager = Group.objects.using(db_alias).create(name='Manager')
+#     Group.objects.using(db_alias).create(name='Staff')
+#
+#     manager.permissions.set((all_logs_perm,))
+#     manager.save()
+#
+#
+# def reverse_func(apps, schema_editor):
+#     Group = apps.get_model("auth", "Group")
+#     db_alias = schema_editor.connection.alias
+#
+#     Group.objects.using(db_alias).filter(name="Manager").delete()
+#     Group.objects.using(db_alias).filter(name="Staff").delete()
+#
+#
+# class Migration(migrations.Migration):
+#
+#     dependencies = [
+#         ('main', '0001_initial'),
+#     ]
+#
+#     operations = [
+#         migrations.RunPython(forwards_func, reverse_func)
+#     ]
--- setup-local.sh
+++ setup-local.sh
@@ -11,6 +11,10 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
+# TODO Ideally this would be a migration
+# I ran into this problem before: https://stackoverflow.com/questions/31735042/adding-django-admin-permissions-in-a-migration-permission-matching-query-does-n
+python manage.py create_flexitime_groups
+
 python manage.py create_test_admin_user
 
 python manage.py create_test_user_data
